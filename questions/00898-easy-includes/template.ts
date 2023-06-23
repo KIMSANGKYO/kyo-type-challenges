e8f76ad125197fee75bf71559b20e278a85f40cc
KIMSANGKYO <sangyoes@naver.com>
2023-06-23 17:07:10 +0900

    :triangular_flag_on_post: easy : 898번 복습 필요

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,16 @@
-type Includes<T extends readonly any[], U> = any
+import { Equal } from "../../utils"
+
+type IsEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false
+
+type Includes<T extends readonly unknown[], U> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? Equal<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false
