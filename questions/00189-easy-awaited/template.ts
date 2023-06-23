00e89ab2ce6ff6e30610d15a46ab76855a3c0581
KIMSANGKYO <sangyoes@naver.com>
2023-06-23 16:15:16 +0900

    :goal_net: easy : 189번 솔브

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,5 @@
-type MyAwaited<T> = any
+type MyAwaited<T> = T extends Promise<any>
+  ? MyAwaited<T extends Promise<infer U> ? U : T>
+  : T extends { then: (onfulfilled: (arg: infer U) => any) => any }
+  ? U
+  : T
