f7d88e5b698e0ac8688aaea7ae88ef2d57176505
KIMSANGKYO <sangyoes@naver.com>
2023-06-26 17:58:02 +0900

    :triangular_flag_on_post: medium : 12번 복습 필요

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,15 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+// 테케 1,2 통과 3 미통과
+// type Chainable<T = {}> = {
+//   option<K extends string, V>(key: K, value: V): Chainable<T & { [P in K]: V }>
+//   get(): T
+// }
+// 테케 1,2 통과 3 미통과
+
+// Omit , Record 활용 재귀 > 테케통과
+type Chainable<T = {}> = {
+  option<K extends string, V>(
+    key: K extends keyof T ? (V extends T[K] ? never : K) : K,
+    value: V
+  ): Chainable<Omit<T, K> & Record<K, V>>
+  get: () => T
 }
