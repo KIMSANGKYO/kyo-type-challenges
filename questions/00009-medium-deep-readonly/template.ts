f020f873d7f3c4edfb8e71323271de34f9570914
KIMSANGKYO <sangyoes@naver.com>
2023-06-26 17:39:00 +0900

    :triangular_flag_on_post: medium : 9번 복습 필요

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,19 @@
-type DeepReadonly<T> = any
+// 테케 2 번 통과 //
+// type DeepReadonly<T> = {
+//     readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K]
+//   }
+
+// 테케 2 번 통과 //
+
+// recommend // 테케 1번통과
+// type DeepReadonly<T> = keyof T extends never
+//   ? T
+//   : { readonly [K in keyof T]: DeepReadonly<T[K]> }
+// recommend // 테케 1번통과
+
+// 위 둘 병합 작성했지만 1번만 통과
+type DeepReadonly<T> = keyof T extends never
+  ? T
+  : {
+      readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K]
+    }
