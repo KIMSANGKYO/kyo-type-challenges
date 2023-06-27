ac84ce32f13548a7db47582a19da0e7fdd141660
KIMSANGKYO <sangyoes@naver.com>
2023-06-27 18:24:53 +0900

    :goal_net: medium : 296번 해설 참고, 복습까지 완료

--- questions/00296-medium-permutation/template.ts
+++ questions/00296-medium-permutation/template.ts
@@ -1,1 +1,18 @@
-type Permutation<T> = any
+// 테케 1,5 통과 2,3,4 미통과
+// type Permutation<T, R = T> = [R] extends [never]
+//   ? []
+//   : T extends infer U | never
+//   ? [U, ...Permutation<Exclude<T, U>>]
+//   : never
+// 테케 1,5 통과 2,3,4 미통과
+// 정렬이 문제인거 같은데
+
+// 해설 참고
+type Permutation<T, K = T> = [T] extends [never]
+  ? []
+  : K extends K
+  ? [K, ...Permutation<Exclude<T, K>>]
+  : never
+
+// T 가 never 면 빈배열
+// K 를 포함한 순열 배열 반환 // 이외는 never
