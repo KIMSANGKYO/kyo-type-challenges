7841735ab29459c11f572c13aa9101a3a15721a8
KIMSANGKYO <sangyoes@naver.com>
2023-06-27 17:34:08 +0900

    :triangular_flag_on_post: medium : 16번 복습 필요

--- questions/00016-medium-pop/template.ts
+++ questions/00016-medium-pop/template.ts
@@ -1,1 +1,12 @@
-type Pop<T extends any[]> = any
+// 테케 3번 미통과
+// type Pop<T extends any[]> = T extends [...infer Rest, any] ? Rest : never
+
+// Expect<Equal<Pop<[]>, []>>
+// 테케 3번 미통과
+
+// 통과 코드 복습필요
+type Pop<T extends any[]> = T["length"] extends 0
+  ? []
+  : T extends [...infer Arg, infer L]
+  ? Arg
+  : never
