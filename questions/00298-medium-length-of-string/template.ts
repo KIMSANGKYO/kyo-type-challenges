eed6f07a969b9f7efdd5ebb48964916ca8eb5b29
KIMSANGKYO <sangyoes@naver.com>
2023-06-27 19:19:29 +0900

    :goal_net: medium : 298번 솔브

--- questions/00298-medium-length-of-string/template.ts
+++ questions/00298-medium-length-of-string/template.ts
@@ -1,1 +1,6 @@
-type LengthOfString<S extends string> = any
+type LengthOfString<
+  S extends string,
+  T extends any[] = []
+> = S extends `${infer _}${infer Rest}`
+  ? LengthOfString<Rest, [...T, any]>
+  : T["length"]
