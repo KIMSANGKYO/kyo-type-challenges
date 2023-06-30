8c316097ef67b4b4c2ec77856af71ff990ecb89a
KIMSANGKYO <sangyoes@naver.com>
2023-06-30 20:49:23 +0900

    :triangular_flag_on_post: medium : 612번 소문자화타입 복습필요

--- questions/00612-medium-kebabcase/template.ts
+++ questions/00612-medium-kebabcase/template.ts
@@ -1,1 +1,16 @@
-type KebabCase<S> = any
+// 케이스 2,8 번 통과
+// type KebabCase<S extends string> = S extends `${infer First}${infer Rest}`
+//   ? `${First extends Uppercase<First>
+//       ? "-"
+//       : ""}${Lowercase<First>}${KebabCase<Rest>}`
+//   : ""
+// 케이스 2,8 번 통과
+
+// 참고  all pass
+type KebabCase<S extends string> = S extends `${infer S1}${infer S2}`
+  ? S2 extends Uncapitalize<S2>
+    ? `${Uncapitalize<S1>}${KebabCase<S2>}`
+    : `${Uncapitalize<S1>}-${KebabCase<S2>}`
+  : S
+
+// Uncapitalize => 전부 소문자로 타입변경
