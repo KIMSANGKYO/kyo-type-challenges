652fa3be271b166bff5b61cf65c6e5fe5d024ff0
KIMSANGKYO <sangyoes@naver.com>
2023-06-30 21:06:07 +0900

    :triangular_flag_on_post: medium : 949번 복습필요

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,30 @@
-type AnyOf<T extends readonly any[]> = any
+// // 테케 9번 미통과, 나머지 통과
+// type AnyOf<T extends readonly any[]> = T extends [infer First, ...infer Rest]
+//   ? true
+//   : false
+// // 테케 9번 미통과, 나머지 통과
+
+// 참고 all pass
+type Bool<T> = T extends 1
+  ? true
+  : T extends string
+  ? T extends ""
+    ? false
+    : true
+  : T extends boolean
+  ? T
+  : T extends any[]
+  ? T["length"] extends 0
+    ? false
+    : true
+  : T extends object
+  ? keyof T extends never
+    ? false
+    : true
+  : false
+
+type AnyOf<T extends readonly any[]> = T extends [infer R, ...infer Rest]
+  ? Bool<R> extends true
+    ? true
+    : AnyOf<Rest>
+  : false
